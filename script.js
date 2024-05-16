1f03d2313532c08919c0cd2edd4eb0a663ba79c2
dinesh22022004 <mayurpatil5458@gmail.com>
2024-05-16 16:51:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Slider functionality (using a library like Swiper.js or Slick.js)
+// Example using Swiper.js:
+var swiper1 = new Swiper('.slider-banner:nth-child(1)', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+
+ {
+    
+}
